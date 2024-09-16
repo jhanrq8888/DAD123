243bdef3e5603bde259363370d19259fb9d93c17
jhanrq8888 <jhanramos8888@gmail.com>
2024-09-16 08:39:40 -0500

    Resiliencia en microservicios

--- ms-pedido/src/main/java/com/example/mspedido/feign/ProductFeign.java
+++ ms-pedido/src/main/java/com/example/mspedido/feign/ProductFeign.java
@@ -1,6 +1,7 @@
 package com.example.mspedido.feign;
 
 import com.example.mspedido.dto.ProductDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,5 +12,9 @@ import java.util.Optional;
 @FeignClient(name = "ms-catalogo-service", path = "/product")
 public interface ProductFeign {
     @GetMapping("/{id}")
-    public ResponseEntity<ProductDto> getById(@PathVariable Integer id);
+    @CircuitBreaker(name = "productoListarPorIdCB", fallbackMethod = "fallbackProductPorId")
+    public ResponseEntity<ProductDto> buscarPorId(@PathVariable(required = true) Integer id);
+    default ResponseEntity<ProductDto> fallbackProductPorId(Integer id, Exception e) {
+        return ResponseEntity.ok(new ProductDto());
+    }
 }
