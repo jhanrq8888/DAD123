8a5f14af84ae498ee75aa2be6adf820865838be5
jhanrq8888 <jhanramos8888@gmail.com>
2024-09-30 09:56:47 -0500

    Implementa Seguridad en microservicios

--- ms-auth/src/main/java/org/example/msauth/repository/AuthUserRepository.java
+++ ms-auth/src/main/java/org/example/msauth/repository/AuthUserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
 public interface AuthUserRepository extends JpaRepository<AuthUser,Integer> {
-    Optional<AuthUser> findByUsername(String username);
+    Optional<AuthUser> findByUserName(String username);
 }
